deb38374aa95718f69d892e6baa92adc44a5b88c
OscarSilvaOfficial <oscarkaka222@gmail.com>
2021-04-30 23:43:41 -0300

    add header

---
+++ src/components/Header.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-toolbar id="menu-header" height="unset" dense>
+    <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+    <v-btn id="icon-bell" icon>
+      <v-icon>mdi-bell</v-icon>
+    </v-btn>
+  </v-toolbar>
+</template>
+<style lang="scss" scoped>
+#menu-header {
+  background-color: #145f6a;
+  display: flex;
+  align-items: center;
+  border-radius: 0;
+
+  i {
+    color: #ffffff;
+  }
+
+  @media (min-width: 601px) {
+    height: 64px !important;
+  }
+
+  @media (max-width: 600px) {
+    height: 48px !important;
+  }
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <section>
+    <Header />
+  </section>
+</template>
+
+<script>
+import Header from "@/components/Header";
+
+export default {
+  name: "index",
+  components: {
+    Header,
+  },
+};
+</script>
